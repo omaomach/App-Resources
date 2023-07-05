54f55455c04cc1a5932726d5faf09a1af8ae42c8
omaomach <aaronjoash44@gmail.com>
2023-07-05 23:57:39 +0300

    worked on menu resource

--- app/src/main/java/com/omao/resourcesapp/MainActivity.kt
+++ app/src/main/java/com/omao/resourcesapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.omao.resourcesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class   MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +14,48 @@ class   MainActivity : AppCompatActivity() {
         // Kotlin
         val color : Int = resources.getColor(R.color.white)
 
+        // Java Menu
 
 
+
+    }
+
+    /**
+     * Initialize the contents of the Activity's standard options menu.  You
+     * should place your menu items in to <var>menu</var>.
+     *
+     *
+     * This is only called once, the first time the options menu is
+     * displayed.  To update the menu every time it is displayed, see
+     * [.onPrepareOptionsMenu].
+     *
+     *
+     * The default implementation populates the menu with standard system
+     * menu items.  These are placed in the [Menu.CATEGORY_SYSTEM] group so that
+     * they will be correctly ordered with application-defined menu items.
+     * Deriving classes should always call through to the base implementation.
+     *
+     *
+     * You can safely hold on to <var>menu</var> (and any items created
+     * from it), making modifications to it as desired, until the next
+     * time onCreateOptionsMenu() is called.
+     *
+     *
+     * When you add items to the menu, you can implement the Activity's
+     * [.onOptionsItemSelected] method to handle them there.
+     *
+     * @param menu The options menu in which you place your items.
+     *
+     * @return You must return true for the menu to be displayed;
+     * if you return false it will not be shown.
+     *
+     * @see .onPrepareOptionsMenu
+     *
+     * @see .onOptionsItemSelected
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.my_menu, menu)
+        return true
     }
 }
