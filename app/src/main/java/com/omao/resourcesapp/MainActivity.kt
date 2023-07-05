0ca2a387a36a95ff39de7b72bc41745b4d24f017
omaomach <aaronjoash44@gmail.com>
2023-07-05 21:51:23 +0300

    worked on colors and strings resources

--- app/src/main/java/com/omao/resourcesapp/MainActivity.kt
+++ app/src/main/java/com/omao/resourcesapp/MainActivity.kt
@@ -3,9 +3,16 @@ package com.omao.resourcesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class   MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Kotlin
+        val color : Int = resources.getColor(R.color.white)
+
+
+
     }
 }
